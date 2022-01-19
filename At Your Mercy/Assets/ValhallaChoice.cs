a301a80b570b5b05f5c820b084f41c83a0f6748f
nosebleed42069 <noamarcon04@gmail.com>
2022-01-19 01:52:31 +0100

    Revert "more ui things"
    This reverts commit 30e5776f96424df0dd010f462356e2e3a48c03b5.

--- At Your Mercy/Assets/ValhallaChoice.cs
+++ At Your Mercy/Assets/ValhallaChoice.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-<<<<<<< Updated upstream
 using UnityEngine.UI;
 
 
@@ -14,14 +13,6 @@ public class ValhallaChoice : MonoBehaviour
     string name;
     
     List<string> nameList = new List<string>();
-=======
-
-public class ValhallaChoice : MonoBehaviour
-{
-
-    public TextMeshProUGUI slain;
-
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
