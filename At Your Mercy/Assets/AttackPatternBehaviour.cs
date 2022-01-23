6becf3555a88bda40ff18f126b2892199fcf77d2
Fabsoll <75846379+Fabsoll@users.noreply.github.com>
2022-01-23 20:51:32 +0100

    lightning attack is ready

--- At Your Mercy/Assets/AttackPatternBehaviour.cs
+++ At Your Mercy/Assets/AttackPatternBehaviour.cs
@@ -13,6 +13,9 @@ public class AttackPatternBehaviour : MonoBehaviour
 
     public float gapBetweenLighting;
 
+    public bool castLightning = false;
+    private bool isRecharged = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,16 +26,19 @@ public class AttackPatternBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.L)){
+        if(castLightning && isRecharged){
             StartCoroutine(SpawnLightningWithDelay());
         }
     }
 
     private IEnumerator SpawnLightningWithDelay(){
+        isRecharged = false;
+        castLightning = false;
         int i = -1;
         Vector3 spawnPos = new Vector3(playerPos.position.x + (i * gapBetweenLighting), LightningAttack.transform.position.y, 0f);
         GameObject lightningInstance = Instantiate(LightningAttack, spawnPos, Quaternion.identity);
         yield return new WaitForSeconds(delay);
+        isRecharged = true;
         i++;
         spawnPos = new Vector3(playerPos.position.x + (i * gapBetweenLighting), LightningAttack.transform.position.y, 0f);
         lightningInstance = Instantiate(LightningAttack, spawnPos, Quaternion.identity);
--- At Your Mercy/Assets/Boss.cs
+++ At Your Mercy/Assets/Boss.cs
@@ -7,15 +7,33 @@ public class Boss : MonoBehaviour
     public bool isFlipped = true;
     public Transform player;
 
+    float time;
+    float timeDelay;
+
+    public Animator bossAnimatorController;
+    public AttackPatternBehaviour attackPatternBehaviour;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("NumberEverySecond", 1f, 1f);
+        time = 0f;
+        timeDelay = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        time = time + 1f * Time.deltaTime;
+        if(time >= timeDelay){
+            time = 0;
+            if(NumberEverySecond() == 1){
+                bossAnimatorController.SetTrigger("LightningAttack");
+                attackPatternBehaviour.castLightning = true;
+                //attackPatternBehaviour.castLightning = false;
+            }
+            //attackPatternBehaviour.castLightning = false;
+        
+        }
     }
 
     public void LookAtPlayer(){
@@ -41,5 +59,10 @@ public class Boss : MonoBehaviour
         int result = Random.Range(min, max);
         Debug.Log(result);
         return result;
+    }
+
+    public IEnumerator Test(){
+        NumberEverySecond();
+        yield return new WaitForSeconds(1f);
     }
 }
