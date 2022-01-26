ab549e970fc7fc290205333567940c04127fdea9
nosebleed42069 <noamarcon04@gmail.com>
2022-01-26 20:59:29 +0100

    bruh

--- At Your Mercy/Assets/Scripts/Player stuff/PlayerMovement.cs
+++ At Your Mercy/Assets/Scripts/Player stuff/PlayerMovement.cs
@@ -145,7 +145,7 @@ public class PlayerMovement : MonoBehaviour
     // }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Ground")){
+        if(other.gameObject.CompareTag("Ground")|| other.gameObject.CompareTag("PlatformGroun")){
             playerAnim.SetBool("isJumping", false);
             onAir = false;
         }
